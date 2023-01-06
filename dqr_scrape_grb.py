744b53b70ee6b04bfba62224f10a4ce5ff68e2a5
navaneethnpk <navaneethpkrasikapriya.npk@gmail.com>
2023-01-06 15:12:02 +0530

    commit0.1

--- dqr_scrape_grb.py
+++ dqr_scrape_grb.py
@@ -9,12 +9,3 @@ dftable = table_dqr[0]
 #print(dftable.head())
 #print(dftable.info())
 
-orbit_id = dftable['Folder']
-obs_id = dftable['OBSID']
-start_time = dftable['Date/time start']
-end_time = dftable['Date/time end']
-for i in range(len(obs_id)):
-	if len(orbit_id[i]) == 43:
-		print(f"{start_time[i]} - {end_time[i]} - {orbit_id[i]} - {obs_id[i]}")
-	else:
-		continue
