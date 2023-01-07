1d331e80ccb598ca2ac95d0d02ae35010c3ad3bc
navaneethnpk <navaneethpkrasikapriya.npk@gmail.com>
2023-01-07 15:39:45 +0530

    code added

--- dqrfolder_grb.py
+++ dqrfolder_grb.py
@@ -1,11 +1,29 @@
 import pandas as pd
 import numpy as np
-import datetime as dt
+from astropy.time import Time
 
 table_dqr = pd.read_html('https://web.iucaa.in/~astrosat/czti_dqr/')
-#print(f'Total tables: {len(table_dqr)}')
-
 dftable = table_dqr[0]
-#print(dftable.head())
-#print(dftable.info())
 
+dftable['Date/time start'] = dftable['Date/time start'].str.replace(' ','T')
+dftable['Date/time end'] = dftable['Date/time end'].str.replace(' ','T')
+
+orbit_id = dftable['Folder']
+obs_id = dftable['OBSID']
+start_time = dftable['Date/time start']
+end_time = dftable['Date/time end']
+
+dftable_first = dftable.head(100)
+
+time_input = input("Enter the Date & Time (YYYY-MM-DDTHH:MM:SS): ")
+given_time = Time(time_input, format='isot', scale='utc')
+
+print(f'               Orbit Folder                           Start                    End        ')
+print(f'------------------------------------------------------------------------------------------')
+for i in range(len(dftable_first)):
+    if len(orbit_id[i]) != 43:
+        continue
+    start = Time(start_time[i], format='isot', scale='utc')
+    end = Time(end_time[i], format='isot', scale='utc')
+    if start <= given_time <= end:
+        print(f'{orbit_id[i]}    {start_time[i]}  -  {end_time[i]}')
