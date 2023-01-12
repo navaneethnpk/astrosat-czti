65fc79f1a52eb8ee6090b6e09a4860e7fea8f19c
navaneethnpk <navaneethpkrasikapriya.npk@gmail.com>
2023-01-12 12:57:02 +0530

    code changed

--- dqrfolder_grb.py
+++ dqrfolder_grb.py
@@ -1,12 +1,10 @@
 import pandas as pd
 import numpy as np
 from astropy.time import Time
+from datetime import datetime, timedelta
 
 table_dqr = pd.read_html('https://web.iucaa.in/~astrosat/czti_dqr/') #Reading the HTML page using pandas
 dftable = table_dqr[0] #Selecting the required table
-
-dftable['Date/time start'] = dftable['Date/time start'].str.replace(' ','T') #Replacing "Space" in the start time entry with "T"
-dftable['Date/time end'] = dftable['Date/time end'].str.replace(' ','T') #Replacing "Space" in the end time entry with "T"
 
 orbit_id = dftable['Folder'] #Orbit folder name
 obs_id = dftable['OBSID'] #Oservation ID
@@ -15,15 +13,15 @@ end_time = dftable['Date/time end'] #Orbit end time
 
 dftable_first = dftable.head(100) #Selecting first 100 orbits
 
-time_input = input("Enter the Date & Time (YYYY-MM-DDTHH:MM:SS): ") #Asking for GRB trigger time in a specific format
-given_time = Time(time_input, format='isot', scale='utc') #Converting the GRB trigger time into Astropy time format
+time_input = input("Enter the Date & Time (YYYY-MM-DD HH:MM:SS): ") #Asking for GRB trigger time in a specific format
+given_time = datetime.strptime(time_input, '%Y-%m-%d %H:%M:%S') #Converting the GRB trigger time into time format
 
-print(f'               Orbit Folder                           Start                    End        ')
-print(f'------------------------------------------------------------------------------------------')
+print(f'               Orbit Folder                            Start                     End        ')
+print(f'--------------------------------------------------------------------------------------------')
 for i in range(len(dftable_first)): #looping in the first 100 orbits
     if len(orbit_id[i]) != 43: #checking and accepting orbit id's with character length = 43. Skipping merged entries
         continue
-    start = Time(start_time[i], format='isot', scale='utc') #Converting orbit start time to Astropy time format
-    end = Time(end_time[i], format='isot', scale='utc') #Converting orbit end time to Astropy time format
+    start = datetime.strptime(start_time[i], '%Y-%m-%d %H:%M:%S') #Converting orbit start time to time format
+    end = datetime.strptime(end_time[i], '%Y-%m-%d %H:%M:%S') #Converting orbit end time to time format
     if start <= given_time <= end: #checking the given GRB trigger time is present in the start and end time duration
-        print(f'{orbit_id[i]}    {start_time[i]}     {end_time[i]}') #Printing the orbit folder name for the GRB trigger time
+        print(f'{orbit_id[i][:37]} {orbit_id[i][-5:]}     {start_time[i]}     {end_time[i]}') #Printing the orbit folder name for the GRB trigger time
